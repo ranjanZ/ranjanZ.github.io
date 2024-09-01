c23b51b4daeedb5cb9ba6fd3b7f9c36693c18ee7
ranjanZ <ranjan.rbc@gmail.com>
2024-09-02 01:48:02 +0530

    streamlit page

---
+++ streamlit_page/blogs.py
@@ -0,0 +1,122 @@
+
+
+def blog_tensor_diff(st):
+    st.write("""
+    Before going into Deep learning, one of the major concepts is needed tensor calculus. To be more specific we need to know how to differentiate a scaler/vector/matrix with respect to vector/matrix. In this blog I am going discuss that will be needed in to understand Deep learning.
+
+    """)
+    st.write("""
+    Differentiation of scalar with respect to vector/Matrix
+    """)
+
+    st.latex(r""" \frac{\partial f(\X)}{\partial \X} = \begin{bmatrix} \frac{\partial f(\X)}{\partial x_1} \\ \frac{\partial f(\X)}{\partial x_2} \\ ..\\ .. \\ \frac{\partial f(\X)}{\partial x_n} \end{bmatrix}""")
+
+    st.write(""" Note: Similarly when you do derivative of a scalar with respect to matrix the output will always be a matrix as the size of the matrix by which you are doing derivative. """)
+
+
+
+
+
+
+
+def VSA(st):
+    # content of the blogs page
+    #st.title("Basics of Traiding and Financial Market")
+    #st.latex(r""" \sum_{k=0}^{n-1} \pi e^{k}""")
+    # Set the page background color
+    st.markdown("""
+        <style>
+        .css-18e3th9 {
+            background-color: #f0f2f6;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    # Create a title
+    st.title("Volume Spread Analysis: A Powerful Tool for Market Insights")
+
+    # Create a subheading
+    st.subheader("Introduction")
+
+    # Create a markdown section
+    st.markdown("""
+    Volume Spread Analysis (VSA) is a technical analysis technique used to identify market trends and patterns by analyzing the relationship between volume and price movements. VSA is based on the idea that volume precedes price movement, and by analyzing volume, traders can gain valuable insights into market sentiment and potential price movements.
+    """)
+
+    # Create a subheading
+    st.subheader("Key Concepts of VSA")
+
+    # Create a markdown section
+    st.markdown("""
+    * **Volume**: The amount of trading activity in a given period.
+    * **Spread**: The difference between the high and low prices of a trading period.
+    """)
+
+    st.subheader("All Cases of VSA")
+    st.write(r"""When I say High or Low, it's relative to other candles. Generally, the spread ($S$) is proportional to Volume ($V$), i.e., $S \propto kV$, where $k$ is a constant.
+When we observe either $S$ or $V$ deviating from their normal range, it indicates an anomaly. Note that $S$ and $V$ are compared to their historical values to determine if they are relatively high or low.
+""")
+    st.markdown("""
+    * **High Volume, High Spread**
+    * **High Volume, Low Spread**
+    * **Low Volume, High Spread**
+    * **Low Volume, Low Spread**
+    """)
+
+
+
+
+    # Create a markdown section
+
+    st.subheader("**High Volume High Spread**")
+    st.image("./data/vsa/HVHS.png", caption="High Volume High Spread",width=800)
+    st.markdown("---")
+
+    st.subheader("**High Volume Low Spread**")
+    st.image("./data/vsa/HVLS.png", caption="High Volume Low Spread",width=800)
+    st.markdown("---")
+
+    st.subheader("**Low Volume High Spread**")
+    st.image("./data/vsa/LVHS.png", caption="Low Volume High Spread",width=800)
+    st.markdown("---")
+
+    st.subheader("**Low Volume Low Spread**")
+    st.image("./data/vsa/LVLS.png", caption="Low Volume Low Spread",width=800)
+    st.markdown("---")
+    st.markdown("---")
+
+    # Create a subheading
+    st.header("How to Apply VSA")
+    st.write("""
+    - Low Volume: The smart money has no interest in the upside.
+    - Higher Volume: Smart money is selling into the public buying.
+    """)
+
+    st.subheader("**High Spread with different Volume**")
+
+
+    st.image("data/vsa/VHS.png", caption="High Spread with different Volume",width=800)
+    st.markdown("---")
+
+
+
+    st.subheader("**Narrow Spread with different Volume**")
+    st.image("data/vsa/VLS.png", caption="Narrow Spread with different Volume",width=800)
+    st.markdown("---")
+
+
+    st.subheader("**Rejection candle with different volume**")
+    st.image("data/vsa/VLS2.png", caption="Narrow Spread rejection with different volume",width=800)
+    st.markdown("---")
+
+    # Create a markdown section
+    st.header("**Aplications**")
+    st.markdown("""
+    * **Early Warning System**: VSA can alert traders to potential price movements before they occur.
+    * **Market Sentiment Insights**: VSA provides valuable insights into market sentiment and trader behavior.
+    * **Improved Trading Decisions**: By analyzing volume and price movements, traders can make more informed trading decisions.
+    """)
+    st.markdown("---")
+
+    st.subheader("**Statistical Analysis and Signal Generation:**")
+
--- streamlit_page/profile.py
+++ streamlit_page/profile.py
@@ -1,42 +1,213 @@
 import streamlit as st
-
+from blogs import *
 
 base_path="https://raw.githubusercontent.com/ranjanZ/ranjanZ.github.io/master/streamlit_page/"
 
-# Page title and layout
-st.set_page_config(page_title="My Streamlit Profile", layout="wide")
+# Page title, icon, and layout
+st.set_page_config(
+    page_title="Ranjan's Profile",
+    page_icon=":robot:",  # Add a robot icon (adjust if desired)
+    layout="wide",
+)
+
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
-tabs = ["About", "Experience", "Skills", "Projects", "Publications", "Blogs", "Contact"]
+tabs = ["About", "Carrier and  Experience", "Projects", "Publications", "Blogs", "Contact","My Story"]
 selected_tab = st.sidebar.selectbox("Select a tab", tabs)
 
-# Main content based on selected tab
+# Custom CSS for background image and styling (optional)
+css="""
+body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background-image: url("path/to/your/background_image.jpg");
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;  
+  # Ensures image stays fixed on scroll
+}
+
+.sidebar .sidebar-content {
+    background-color: rgba(0, 0, 0, 0.7);  # Semi-transparent sidebar background
+    color: white;
+}
+
+.st-cb {  # Adjust padding for "Select a tab" based on your font size
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+
+.st-sh {  # Adjust header font size based on your preference
+    font-size: 2.5em;
+}
+
+.st-t {  # Adjust body text size based on your preference
+    font-size: 1.1em;
+    line-height: 1.5;
+}
+"""
+
+st.markdown("<style>" + css + "</style>", unsafe_allow_html=True)
+
 if selected_tab == "About":
     st.header("About")
-    st.image(base_path+"data/profile_picture.jpg")  # Replace with your image path
-    st.write("Applied AI/ML | Machine Learning Researcher")
-    st.write("Email: ranjan.rev@gmail.com | Phone: +91 6290153508")
-    st.write("Website: https://ranjanz.github.io/")
+    st.image(base_path + "data/profile_picture.jpg")  # Replace with your image path
 
-elif selected_tab == "Experience":
-    st.header("Experience")
-    experience_data = [
-        ("JP Morgan Chase & Co", "Bangalore, India", "Jan 2023 - Present", "Applied AI/ML as AVP, Working on applied AI/ML in financial domain..."),
-        ("Samsung Research", "Bangalore, India", "2021 - 2023", "Lead Research Engineer, Worked on state-of-the-art video panoptic segmentation...")
+
+    # About text
+    st.write("""
+    Currently, I am an applied AI/ML industry researcher at JP Morgan Chase & Co, Bangalore, India
+
+    Beyond my professional pursuits, I'm fascinated by:
+    - Entrepreneurship: Turning ideas into impact
+    - Algorithmic Trading: Where finance meets technology
+    - Robotics & Drones: Unleashing autonomous possibilities
+    """)
+
+    # Links to profiles
+    st.write("""
+    [:linkedin: LinkedIn]((https://www.linkedin.com/in/ranjan-mondal-176b0362/))
+    [:google-scholar: Google Scholar](https://scholar.google.com/citations?user=JWuX_HsAAAAJ&hl=en)
+    [:github: GitHub](https://github.com/ranjanZ)
+    """)
+
+
+
+
+
+
+    st.subheader("Beyond the Maths, Stats, and Code")
+    st.write(
+        """
+        - **My goal and Social Impact:** I believe that in India, the rich are getting richer while the poor are getting poorer, I want to reduce this gap. A farmer gives all his hard work to make the rich people richer, but they receive nothing. I am confident that I can make a positive change, even if it's only for a small percentage of our population.
+
+        - **Algorithmic Trading:** Where finance meets technology. I explore statistical arbitrage, backtesting, and finding an edge in the Indian market for derivatives as well as equity.
+
+        - **Robotics & Drones:** I love to play with drones and electronics.
+        - **Travelling:** Australia, Thailand, Porto.
+        """
+    )
+
+    st.write("Let's connect and shape the future of AI/ML together!")
+
+
+
+    st.header("Updates")
+
+
+    # Create a list of your talks and tutorials with links
+    talks_and_tutorials = [
+        ("July 2024", "Invited talk at SERB Sponsored Workshop", "Deep Learning: Foundation to Cutting Edge Technologies", "UPES, Dehradun, India", "https://www.linkedin.com/feed/update/urn:li:activity:7218653272482471936/"),
+        ("Nov 2022", "Accepted full paper at BMVC ", "Morphological Network: How Far Can We Go with Morphological Neurons?","London, UK", "https://bmvc2022.mpi-inf.mpg.de/"),
+        ("March 2022", "Invited talk", "Learning with structuring elements", "Winter School on Deep Learning, ISI, Kolkata, India", "https://sites.google.com/view/wsdl2022/home?authuser=0"),
+        ("March 2020", "Invited talk", "Deep Learning", "Indian Institute of Information Technology (IIIT), Guwahati, India", "https://www.iiitg.ac.in/event?per_page=1"),
+        ("July 2019", "Tutorial", "Data Visualization using Python", "ISI-ICTP Summer School on Internet of Things, Indian Statistical Institute, Kolkata", "https://www.isical.ac.in/~ss_iot/"),
+        ("Jan. 2019", "Invited talk", "Object Supervised Learning to Adversarial Learning", "Techno Main, Salt Lake, Faculty Development Program on Image and Vision Computing", ""),
+        ("June. 2018", "Tutorial", "Convolutional Neural Network and Autoencoder", "Indian Statistical Institute (Kolkata), Fourth Summer School on Computer Vision, Graphics and Image Processing", "https://www.isical.ac.in/~ecsu/?q=CFP"),
+        ("April. 2017", "Invited talk", "Python", "Midnapore college (Medinipur, West Bengal), Workshop in Python", "https://midnaporecollege.ac.in/"),
+        ("Feb. 2016", "Invited talk", "System Administration", "University of Calcutta, IEEE Electron Devices Society sponsored workshop in Python and System Administration", ""),
     ]
 
-    for company, location, dates, description in experience_data:
-        with st.expander(company):
-            st.write(f"**Location:** {location}")
-            st.write(f"**Dates:** {dates}")
-            st.write(description)
 
-elif selected_tab == "Skills":
-    st.header("Skills")
-    skills_data = ["Python", "Machine Learning", "Deep Learning", "Computer Vision", "Data Science", "Natural Language Processing"]
+    # Display the talks and tutorials as bullet points with links
+    for year, talk_type, topic, location, link in talks_and_tutorials:
+        st.markdown(f"- **{year}**: {talk_type} on **{topic}** at **{location}** [Link]({link})")
 
-    st.write(" ".join([f'<span class="badge badge-pill badge-primary">{skill}</span>' for skill in skills_data]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+elif selected_tab == "Carrier and  Experience":
+  st.header("**🚀 Experience**")
+
+  industry_experience_data = [
+    (
+      "JP Morgan Chase & Co.",
+      "Bangalore, India",
+      "Jan 2023 - Present",
+      """Applied AI/ML as AVP. Working on applied AI/ML in financial domain. Specifically, working on large scale graph learning, mathematical modeling of financial optimization problems, and large language models.""",
+    ),
+    (
+      "Samsung Research",
+      "Bangalore, India",
+      "2021 - 2023",
+      """Lead Research Engineer. Worked on state-of-the-art video panoptic segmentation problems for different embedded platforms. It involves real time instance tracking, temporal stability accurate mask prediction for video.""",
+    ),
+    (
+      "Advanced Micro Devices (AMD), Bengaluru, India",  # Company
+      "June 2014",  # Dates (assuming it was a one-month internship)
+      "Summer Intern",  # Location
+      """Mentored by Dr. Anasua Bhowmik (Principal Member of Technical Staff). [Brief description of your internship experience here]. Include details like the projects you worked on and the skills you gained."""  # Description
+    ),
+  ]
+
+  academic_experience_data = [
+    (
+      "Indian Statistical Institute (ISI), Kolkata, India",
+      "2015 - 2021",
+      """Doctor of Philosophy in Machine Learning and Computer Vision""",
+      """ Thesis: Morphological Network: Learning with Morphological Neurons. Supervisor: Prof. Bhabatosh Chanda.""",
+    ),
+    (
+      "Indian Statistical Institute (ISI), Kolkata, India",
+      "2013 - 2015",
+      "Master of Technology (M.Tech), Computer Science",
+      """Percentile: First Division with Distinction - 76%. Dissertation: Unified Pre-fabrication and Post-fabrication Hardware Security. Supervisor: Prof. Susmita Sur-Kolay.""",
+    ),
+    (
+      "Kalyani Government Engineering College, Kalyani, India",
+      "2009 - 2013",
+      "Bachelor of Technology (B.Tech), Computer Science and Engineering",
+      """DGPA: 8.08. Project: Linux Kernel and File System Development.""",
+    ),
+  ]
+
+  st.subheader("🚀 Industry Experience")
+  for company, dates, location, description in industry_experience_data:
+    with st.expander(company, expanded=True):
+      st.write(f"**Location:** {location}")
+      st.write(f"**Dates:** {dates}")
+      st.write(description)
+
+  st.subheader("🎓 Academic Experience")
+  for company, dates, location, description in academic_experience_data:
+    with st.expander(company, expanded=True):
+      st.write(f"**Location:** {location}")
+      st.write(f"**Dates:** {dates}")
+      st.write(description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 elif selected_tab == "Projects":
     st.header("Projects")
@@ -51,36 +222,160 @@ elif selected_tab == "Projects":
             st.write(f'<a href="{link}">GitHub Link</a>', unsafe_allow_html=True)
 
 elif selected_tab == "Publications":
-    st.header("Publications")
-    publications_data = [
-        ("Morphological Network: How Far Can We Go with Morphological Neurons?", "Ranjan Mondal, Sanchayan Santra, Soumendu Sundar Mukherjee, and Bhabatosh Chanda", "https://arxiv.org/abs/2208.09790"),
-        # Add more publications here
-    ]
+    st.title("Publications and Preprints")
+    st.write("[Google Scholar](https://scholar.google.com/citations?user=JWuX_HsAAAAJ&hl=en)", unsafe_allow_html=True)
 
-    for title, authors, link in publications_data:
-        with st.expander(title):
-            st.write(authors)
-            st.write(f'<a href="{link}">Preprint</a>', unsafe_allow_html=True)
+
+
+
 
 elif selected_tab == "Blogs":
-    st.header("Blogs")
-    blog_posts = [
-        ("Blog Post 1", "Link to Blog Post 1"),
-        ("Blog Post 2", "Link to Blog Post 2"),
-        # Add more blog posts here
+    st.title("Ranjan's Blog")
+    st.write("A collection of thoughts and insights on AI, ML, Trading and more.")
+
+
+    # Blog content section
+    st.markdown("---")  # Horizontal separator
+    # Looping through articles (replace with actual data)
+    articles = [
+        {
+            "title": "Tensor differentiation",
+            "excerpt": "Before going into Deep learning, one of the major concepts is needed tensor calculus...",
+            "link": "",
+            "date": "2016-04-28",
+            "author": "Ranjan M.",
+            "all content": blog_tensor_diff,
+
+        },
+        {
+            "title": "Volume Spread analysis",
+            "excerpt": "Volume Spread Analysis (VSA) is a technical analysis technique used to identify market trends and patterns by analyzing..",
+            "date": "2024-07-21",
+            "author": "Ranjan M.",
+            "all content": VSA,
+        },
+         {
+            "title": "Arbitrage: Making Free Money from Market!! Just Wait for the Opportunity",
+            "excerpt": "Coming....",
+            "date": "2024-10-20",
+            "author": "Ranjan M.",
+            "all content":"" ,
+        },
+
+
     ]
 
-    for title, link in blog_posts:
-        st.write(f'<a href="{link}">{title}</a>', unsafe_allow_html=True)
+
+
+    def render_blog_post(title, excerpt, date, author, all_content):
+        st.subheader(title)
+        st.markdown(f"**{date}** | {author}")
+        st.markdown(excerpt)
+
+        if st.button("Read More", key=date):
+            st.markdown(all_content(st))
+
+        st.markdown("---")  # Horizontal separator
+
+
+
+
+    # Looping through articles
+    for article in articles:
+        render_blog_post(article["title"], article["excerpt"], article["date"], article["author"], article["all content"])
+
+
+
+    # Pagination (example using placeholders)
+    #st.markdown("**Page 1 of 2**")
+    #st.markdown("[Older Posts &raquo;](placeholder_older_posts_link)", unsafe_allow_html=True)  # Replace with actual link
+
+    # Footer section
+    st.markdown("---")  # Horizontal separator
+    st.markdown("© ranjan's blog")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 elif selected_tab == "Contact":
     st.header("Contact")
     st.write("**Email:** ranjan.rev@gmail.com")
-    st.write("**Phone:** +91 6290153508")
+    st.write("**Phone:** +91 6290153xxx -- you have to try 10^3 times")
     st.write("**Website:** https://ranjanz.github.io/")
+
+
+
+elif selected_tab == "My Story":
+    st.header("My Story")
+    st.write(
+        """
+    Applied Research Scientist 
+    As a child, I dreamed of becoming a cricketer or an astronaut, but growing up in a lower middle-class family, I soon faced the harsh realities of life. I scaled down my ambitions and set my sights on becoming a primary school teacher, with a modest salary of ₹12,000/month. However, fate had other plans, and an injury crushed my cricketing aspirations.
+
+    Until the 8th grade, I was an unenthusiastic student, believing that academic success wouldn't guarantee a bright future. But everything changed when I discovered my passion for mathematics, particularly geometry, and solving Olympiad problems, inspired by my brilliant friend Soumendu.
+
+    From that moment on, I began my journey, and I'm still amazed at how far I've come. My story is one of perseverance, self-discovery, and the power of embracing one's true potential."
+
+    I believe I don't have much natural brainpower, but I've learned to compensate for it with sheer hard work. I firmly believe that anyone can compensate for intelligence with hard work.
+
+    """
+    )
+
+
 
 # Footer
 st.write("© 2024 Ranjan Mondal")
+
+
 
 
 
